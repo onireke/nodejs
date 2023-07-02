3340abab9583d5b7270ea739565b6e419191a58f
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-07-02 19:05:19 +0100

    nodejs

---
+++ app.js
@@ -0,0 +1,7 @@
+// function sayHello(name) {
+//   console.log(name);
+// }
+
+// sayHello("abdulfatah");
+const logger = require("./logger");
+logger.log("message");
---
+++ logger.js
@@ -0,0 +1,8 @@
+let url = "http:/blessedcoder.com/log";
+
+function log(message) {
+  // send HTTP
+  console.log(message);
+}
+
+module.exports.log = log;
