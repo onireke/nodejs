731183d85a0790fcca5c27d65a82f018f6dce209
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-07-04 14:44:07 +0100

    nodejs

--- app.js
+++ app.js
@@ -4,10 +4,37 @@
 
 // sayHello("abdulfatah");
 // const logger = require("./logger");
-// logger.log("message");
+// // logger.log("message");
 
-const path = require("path");
+// const path = require("path");
 
-const pathObj = path.parse(__filename);
+// const pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+// const os = require("os");
+
+// let totalMemory = os.totalmem();
+// let freeMemory = os.freemem();
+
+// console.log("Total Memory:" + totalMemory);
+
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello world");
+    res.end();
+  }
+
+  if (req.url === "/api/coorses") {
+    res.write(JSON.stringify([A, B, C, D, E]));
+    res.end();
+  }
+});
+server.listen(3000);
+
+console.log("listening on port 3000...");
